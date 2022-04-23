db53d347b08a3aea33af551693c16bdb21b53fa8
Patrick Bauer <patrick1bauer@gmail.com>
2022-04-23 18:40:09 -0400

    Initial Commit.

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+echo 'patrickbauer.net' > CNAME
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:patrick1bauer/portfolio.git main:gh-pages
+
+cd -
---
+++ main.js
@@ -0,0 +1,6 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Patrick's portfolio!!!</h1>
+  <a href="https://www.linkedin.com/in/patrickbauer/">Linkedin</a>
+`
