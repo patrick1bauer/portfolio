115ab1819ebf28b169c3bebc5adac51b9d76517e
Patrick Bauer <patrick1bauer@gmail.com>
2022-04-24 00:05:53 -0400

    Added html text.

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <h1>Patrick's portfolio!!!</h1>
+  <p>add me on linked in:</p>
   <a href="https://www.linkedin.com/in/patrickbauer/">Linkedin</a>
 `
