d002567bc1830fc1fc382a402b38c075dfa140bc
Patrick Bauer <patrick1bauer@gmail.com>
2023-09-20 19:59:48 -0400

    Updated World Travels section on about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -10,7 +10,7 @@ export default function About() {
         </p2>
         <br />
         <h1>I Bleed Bleeding-Edge Technology</h1>
-        <h2>Super Cool Projects</h2>
+        <h2>Experience</h2>
         <p2>Cross Currency Global Mass Payments <a href="https://www.jpmorgan.com/payments/payments-and-cross-currency-solutions/cross-currency-solutions" target="_blank">@JPMorgan Chase & Co.</a></p2>
         <br />
         <p2>Missile Defense Systems <a href="https://www.rtx.com/raytheon/what-we-do/strategic-missile-defense" target="_blank">@Cummings Aerospace / Raytheon</a></p2>
@@ -28,9 +28,7 @@ export default function About() {
         <br />
         <h1>Epic Voyages & Thrilling Ventures</h1>
         <h2>World Travels</h2>
-        <p2>USA, Mexico, Canada, Peru, South Africa, Poland, Czechia, United Kingdom, Germany, Austria, Spain, Hungary, Switzerland, Italy, Romania, France, Belgium, Netherlands, Turkey</p2>
-        <br />
-        <p2>Upcoming: Dominican Republic</p2>
+        <p2>USA, Mexico, Canada, Peru, South Africa, Poland, Czechia, United Kingdom, Germany, Austria, Spain, Hungary, Switzerland, Italy, Romania, France, Belgium, Netherlands, Turkey, Dominican Republic</p2>
         <br />
         <h2>Music</h2>
         <p2>Life In Color Miami 2014, Defqon1 2023</p2>
