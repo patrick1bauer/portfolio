6ad000d7c61b2edcdede4df76f045fec706b77e6
patrick1bauer <patrick1bauer@gmail.com>
2025-03-07 15:15:49 -0500

    Added meta tag to page

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,6 +6,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
     <Head>
+      <meta name="Porfolio page for Patrick Bauer. "/>
       <title>Patrick Bauer</title>
       <link rel="icon" type="image/png" href="/images/favicon.png"></link>
     </Head>
